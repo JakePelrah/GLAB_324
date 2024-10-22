64265f05172872e2ba3b0b57dc4df513b30946d2
Jake Pelrah <pelrahjsp@gmail.com>
2024-10-22 10:38:51 -0400

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
 } from 'react-router-dom'
 
-import { Navbar } from './app/Navbar.jsx'
-import { PostsList } from './features/posts/PostsList.jsx'
-import { AddPostForm } from './features/posts/AddPostForm.jsx'
+import { Navbar } from './app/Navbar'
+
+import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
+import { EditPostForm } from './features/posts/EditPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
+import { NotificationsList } from './features/notifications/NotificationsList.jsx'
 
 function App() {
   return (
@@ -20,16 +26,22 @@ function App() {
             exact
             path="/"
             render={() => (
-              <>
-              <AddPostForm/>
+              <React.Fragment>
+                <AddPostForm />
                 <PostsList />
-              </>
+              </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
+          <Route exact path="/notifications" component={NotificationsList} />
           <Redirect to="/" />
         </Switch>
       </div>
     </Router>
   )
 }
+
 export default App
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,20 +1,83 @@
-import { createSlice } from '@reduxjs/toolkit'
-
-const initialState = [
-    { id: '1', title: 'First Post!', content: 'Hello!' },
-    { id: '2', title: 'Second Post', content: 'More text' }
-]
-
-const postsSlice = createSlice({
+import {
+    createSlice,
+    createAsyncThunk,
+    createSelector,
+    createEntityAdapter,
+  } from '@reduxjs/toolkit'
+  import { client } from '../../api/client'
+  
+  const postsAdapter = createEntityAdapter({
+    sortComparer: (a, b) => b.date.localeCompare(a.date),
+  })
+  
+  const initialState = postsAdapter.getInitialState({
+    status: 'idle',
+    error: null,
+  })
+  
+  export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await client.get('/fakeApi/posts')
+    return response.data
+  })
+  
+  export const addNewPost = createAsyncThunk(
+    'posts/addNewPost',
+    async (initialPost) => {
+      const response = await client.post('/fakeApi/posts', initialPost)
+      return response.data
+    }
+  )
+  
+  const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        postAdded(state, action) {
-            state.push(action.payload)
+      reactionAdded(state, action) {
+        const { postId, reaction } = action.payload
+        const existingPost = state.entities[postId]
+        if (existingPost) {
+          existingPost.reactions[reaction]++
         }
-    }
-})
-
-export const { postAdded } = postsSlice.actions
-
-export default postsSlice.reducer
+      },
+      postUpdated(state, action) {
+        const { id, title, content } = action.payload
+        const existingPost = state.entities[id]
+        if (existingPost) {
+          existingPost.title = title
+          existingPost.content = content
+        }
+      },
+    },
+    extraReducers(builder) {
+      builder
+        .addCase(fetchPosts.pending, (state, action) => {
+          state.status = 'loading'
+        })
+        .addCase(fetchPosts.fulfilled, (state, action) => {
+          state.status = 'succeeded'
+          // Add any fetched posts to the array
+          postsAdapter.upsertMany(state, action.payload)
+        })
+        .addCase(fetchPosts.rejected, (state, action) => {
+          state.status = 'failed'
+          state.error = action.error.message
+        })
+        .addCase(addNewPost.fulfilled, postsAdapter.addOne)
+    },
+  })
+  
+  export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
+  
+  export default postsSlice.reducer
+  
+  export const {
+    selectAll: selectAllPosts,
+    selectById: selectPostById,
+    selectIds: selectPostIds,
+  } = postsAdapter.getSelectors((state) => state.posts)
+  
+  export const selectPostsByUser = createSelector(
+    [selectAllPosts, (state, userId) => userId],
+    (posts, userId) => posts.filter((post) => post.user === userId)
+  )
+  
